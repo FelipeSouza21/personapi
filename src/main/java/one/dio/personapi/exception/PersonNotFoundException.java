2cfbd5ab486b66031abeb316d97799da61a70a95
FelipeSouza21 <fmaximianos@gmail.com>
2021-07-01 15:29:03 -0300

    Fixed exception message

--- src/main/java/one/dio/personapi/exception/PersonNotFoundException.java
+++ src/main/java/one/dio/personapi/exception/PersonNotFoundException.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PersonNotFoundException extends Exception {
+
     public PersonNotFoundException(Long id) {
-        super("Person not found with ID " + id);
+        super(String.format("Person with ID %s not found!", id));
     }
 }
