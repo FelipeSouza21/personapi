ab8332b66a4a5e919b9edd78b03651fdfd239c81
FelipeSouza21 <fmaximianos@gmail.com>
2021-07-12 12:22:47 -0300

    Changed CORS origin

--- src/main/java/one/dio/personapi/controller/PersonController.java
+++ src/main/java/one/dio/personapi/controller/PersonController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+
 @CrossOrigin(origins = {"https://person-crud-sa.firebaseapp.com/"})
 @RestController
 @RequestMapping("/api/v1/people")
